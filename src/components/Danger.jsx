2e43a373e788e6e4b314149478ffa0f8863159c7
kvin-stphns <kvin.stphns@gmail.com>
2024-09-24 15:58:08 -0400

    Danger Zone (New Lyfe)

--- src/components/Danger.jsx
+++ src/components/Danger.jsx
@@ -6,7 +6,7 @@ const Zone = lazy(() => import("@elements/Danger/Zone"));
 
 const VSCode = () => {
 	return (
-		<Default heading="Avicii - The Nights (Official Music Video)" programName="YouTube">
+		<Default heading="Caine Casket - New Lyfe (Official Video)" programName="YouTube">
 			<Suspense fallback={<Loader />}>
 				<Zone />
 			</Suspense>
--- src/elements/Danger/Zone.jsx
+++ src/elements/Danger/Zone.jsx
@@ -1,19 +1,17 @@
 import React from "react";
 import styled from "styled-components";
 import BodyContent from "@elements/Window/BodyContent";
+// import Iframe from "react-iframe";
 
 const Iframe = styled.iframe`
+	height: 100%;
 	width: 100%;
 	border: none;
 `;
 const Zone = () => {
 	return (
 		<BodyContent>
-			<Iframe
-				src="https://www.youtube.com/watch?v=UtF6Jej8yb4"
-				frameborder="0"
-				allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-			></Iframe>
+			<Iframe width="560" height="315" src="https://www.youtube.com/embed/7yq49KZ8YaQ?si=rdOA1a-viK13XoL-" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></Iframe>
 		</BodyContent>
 	);
 };
