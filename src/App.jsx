84162a3c91c581f3accc44d5fe434a1b046eefcb
kvin-stphns <kvin.stphns@gmail.com>
2024-09-24 19:10:44 -0400

    Vercel Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import { ThemeProvider } from "styled-components";
 import colors from "./styles/colors";
 import Routes from "./pages/Routes";
 import DataProvider from "../src/contexts/Data/DataProvider";
+import { Analytics } from "@vercel/analytics/react";
 
 const App = () => {
 	return (
 		<ThemeProvider theme={colors}>
 			<DataProvider>
 				<Routes />
+				<Analytics />
 			</DataProvider>
 		</ThemeProvider>
 	);
